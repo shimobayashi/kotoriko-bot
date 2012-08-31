0f66edc54d1984146b082ea02669748187e5babc
Akimasa Shimobayashi <akimasa@shimobayashi.name>
2012-08-31 16:38:02 +0900

    initial commit

---
+++ fetcher.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'mechanize'
+
+class Fetcher
+  def initialize
+    @agent = Mechanize.new
+  end
+
+  def fetch(dir)
+    @agent.get 'http://t-proj.com/twitter/?q=from%3Akotoriko+%40todesking'
+    @agent.page.search('//div[@class="twitter_status"]').each do |status|
+      next if status.has_attribute?('style')
+      status.to_s =~ /http:\/\/twitter\.com\/intent\/retweet\?tweet_id=(\d+)/
+      id = $1
+      status.inner_text =~ /(.+)From: kotoriko/
+      text = $1
+      p id
+      open(dir + id + '.txt', 'w').write(text)
+    end
+  end
+end
+
+if __FILE__ == $0
+  Fetcher.new.fetch(ARGV[0])
+end
---
+++ updater.rb
@@ -0,0 +1,26 @@
+require_relative 'fetcher'
+require_relative 'parser'
+require_relative 'indexer'
+require_relative 'scorer'
+require_relative 'starter'
+
+class Updater
+  def update(txtDir, parsedFilename, indexFilename, scoreFilename, startFilename)
+    Fetcher.new.fetch(txtDir)
+    f = open(parsedFilename, 'w'); f.write(Marshal.dump(Parser.new.parseDir(txtDir))); f.close
+    f = open(indexFilename, 'w'); f.write(Marshal.dump(Indexer.new(3).indexingFile(parsedFilename))); f.close
+    f = open(scoreFilename, 'w'); f.write(Marshal.dump(Scorer.new.calcFile(parsedFilename))); f.close
+    f = open(startFilename, 'w'); f.write(Marshal.dump(Starter.new.calcFile(indexFilename))); f.close
+  end
+end
+
+if __FILE__ == $0
+  dir = File.dirname(File.expand_path(__FILE__))
+  args = []
+  args << dir + '/tweets/'
+  args << dir + '/tweets.parse'
+  args << dir + '/tweets.index'
+  args << dir + '/tweets.score'
+  args << dir + '/tweets.start'
+  Updater.new.update(*args)
+end
